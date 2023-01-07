337dcc9e2b1dda2838c33d941c0ce4430838aa25
Shahriar <yazdipour@users.noreply.github.com>
2023-01-07 18:57:31 +0100

    feat/get_conn_string

--- src/mysql_proxy.py
+++ src/mysql_proxy.py
@@ -1,56 +1,67 @@
 import mysql.connector
 import sqlite3
 import uuid
+import re
 
 
 class MySQLProxy:
 
-    def __init__(self, mysql_db_name, mysql_db_user, mysql_db_password, mysql_db_host, mysql_db_port):
-        self.mysql_db_name = mysql_db_name
-        self.mysql_db_user = mysql_db_user
-        self.mysql_db_password = mysql_db_password
-        self.mysql_db_host = mysql_db_host
-        self.mysql_db_port = mysql_db_port
+    def __init__(self, connection_string):
 
-    def convert(self):
-        # Connect to the MySQL database
-        mysql_conn = mysql.connector.connect(user=self.mysql_db_user,
-                                             password=self.mysql_db_password,
-                                             host=self.mysql_db_host,
-                                             port=self.mysql_db_port,
-                                             database=self.mysql_db_name)
+        # Compile the regular expression pattern
+        pattern = re.compile(
+            r"mysql://(?P<user>.+):(?P<password>.+)@(?P<host>.+):(?P<port>\d+)/(?P<database>.+)")
 
-        mysql_cursor = mysql_conn.cursor()
+        # Match the connection string against the pattern
+        match = pattern.match(connection_string)
 
-        # Connect to the SQLite database
-        sqlite_name = {str(uuid.uuid4().hex)}.sqlite
-        sqlite_db_path = f'./database/{sqlite_name}'
-        sqlite_conn = sqlite3.connect(sqlite_db_path)
-        sqlite_cursor = sqlite_conn.cursor()
+        # Extract the individual components from the match object
+        config = {
+            "host": match["host"],
+            "port": int(match["port"]),
+            "user": match["user"],
+            "password": match["password"],
+            "database": match["database"],
+        }
+        self.mysql_conn = mysql.connector.connect(
+            user=config["user"],
+            password=config["password"],
+            host=config["host"],
+            port=config["port"],
+            database=config["database"],
+        )
 
-        # Retrieve the list of tables in the MySQL database
-        mysql_cursor.execute("SHOW TABLES")
-        tables = mysql_cursor.fetchall()
 
-        # Iterate through the list of tables and create them in the SQLite database
-        for table in tables:
-            table_name = table[0]
+def convert(self):
+    mysql_cursor = self.mysql_conn.cursor()
+    # Connect to the SQLite database
+    sqlite_name = f'{str(uuid.uuid4().hex)}.sqlite'
+    sqlite_db_path = f'./database/{sqlite_name}'
+    sqlite_conn = sqlite3.connect(sqlite_db_path)
+    sqlite_cursor = sqlite_conn.cursor()
 
-            # Retrieve the structure of the table
-            mysql_cursor.execute(f"DESCRIBE {table_name}")
-            structure = mysql_cursor.fetchall()
+    # Retrieve the list of tables in the MySQL database
+    mysql_cursor.execute("SHOW TABLES")
+    tables = mysql_cursor.fetchall()
+    # Iterate through the list of tables and create them in the SQLite database
+    for table in tables:
+        table_name = table[0]
 
-            # Create the table in the SQLite database
-            sql = f"CREATE TABLE {table_name} ("
-            for column in structure:
-                column_name = column[0]
-                column_type = column[1]
-                sql += f"{column_name} {column_type}, "
-            sql = f"{sql[:-2]})"
-            sqlite_cursor.execute(sql)
+        # Retrieve the structure of the table
+        mysql_cursor.execute(f"DESCRIBE {table_name}")
+        structure = mysql_cursor.fetchall()
 
-        # Save the changes to the SQLite database and close the connections
-        sqlite_conn.commit()
-        mysql_conn.close()
-        sqlite_conn.close()
-        return sqlite_name
+        # Create the table in the SQLite database
+        sql = f"CREATE TABLE {table_name} ("
+        for column in structure:
+            column_name = column[0]
+            column_type = column[1]
+            sql += f"{column_name} {column_type}, "
+        sql = f"{sql[:-2]})"
+        sqlite_cursor.execute(sql)
+
+    # Save the changes to the SQLite database and close the connections
+    sqlite_conn.commit()
+    self.mysql_conn.close()
+    sqlite_conn.close()
+    return sqlite_name
